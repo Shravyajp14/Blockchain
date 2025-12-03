286e6b4764e4f4070e19a7033b09a382a0290c77
Shravya J Poojari <136490723+Shravyajp14@users.noreply.github.com>
2025-12-03 17:44:12 +0530

    FreshFoodSupplyChain.sol

--- FreshFoodSupplyChain.sol
+++ FreshFoodSupplyChain.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.19;
 
 /// @title FreshFoodSupplyChain
-/// @author ChatGPT
 /// @notice Tracks perishable produce from farmer -> consumer with escrow and environmental logging
 contract FreshFoodSupplyChain {
 
